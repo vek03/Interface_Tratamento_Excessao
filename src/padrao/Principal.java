221931c3276719da39fac939c7c1c30e327e7a83
Vek03 <victor.cardoso36@etec.sp.gov.br>
2022-05-30 00:37:25 -0400

    Netbeans 12.5

---
+++ src/padrao/Principal.java
@@ -0,0 +1,22 @@
+package padrao;
+
+/**
+ *
+ * @author Victor Cardoso
+ * Vek Histories
+ */
+public class Principal 
+{
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) 
+  {
+    Aluno aluno = new Aluno();
+        
+    aluno.cadastrarAluno();
+        
+    aluno.exibirDados();
+  }
+  
+}
